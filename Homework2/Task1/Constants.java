be2ddc3ac9aead584054ced4cc0620cf9eeec30f
SvetonosovS <tris23@bk.ru>
2023-09-15 23:59:58 +0300

    task1

---
+++ Homework2/Task1/Constants.java
@@ -0,0 +1,7 @@
+package Homework2.Task1;
+
+public interface Constants {
+    Integer CAT_AGE = 2;
+    String CAT_NAME = "Мурзик";
+    String OWNER_NAME = "Костя";
+}
---
+++ Homework2/Task1/Owner.java
@@ -0,0 +1,21 @@
+package Homework2.Task1;
+
+public class Owner {
+    private String ownerName;
+
+    public Owner(String name) {
+        this.ownerName = name;
+    }
+
+    public Owner() {
+        this(null);
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+}
