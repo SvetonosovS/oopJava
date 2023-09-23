a488aebcec320c02aef333eafc37f9b56cb0cfae
SvetonosovS <tris23@bk.ru>
2023-09-24 01:03:27 +0300

    tasks

---
+++ hw/task3/Main.java
@@ -0,0 +1,17 @@
+package hw.task3;
+
+/***
+ * в данном коде применен принцип Single Responsibility
+ */
+public class Main {
+    public static void main(String[] args) {
+        NumberContainer<Integer> intContainer = new NumberContainer<>();
+        NumberContainer<Double> doubleContainer = new NumberContainer<>();
+
+        intContainer.setNumber(8);
+        doubleContainer.setNumber(2.2);
+
+        System.out.println("Квадрат целого числа: " + intContainer.square(intContainer.getNumber()));
+        System.out.println("Квадрат вещественного числа: " + doubleContainer.square(doubleContainer.getNumber()));
+    }
+}
---
+++ hw/task5/Main1.java
@@ -0,0 +1,9 @@
+package hw.task5;
+
+// применен Lyskov
+public class Main1 {
+    public static void main(String[] args) {
+        UsGen usGen = new UsGen();
+        usGen.test();
+    }
+}
